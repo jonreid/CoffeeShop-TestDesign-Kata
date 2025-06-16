ebd5efc6937df28c1503867c1f4228820f09bb88
Peter Fichtner <1958485+pfichtner@users.noreply.github.com>
2025-06-16 11:59:17 +0200

    Java: Fixed imports in ParameterizedClassTests

--- java/src/test/java/org/sammancoaching/parameterizedtests/concept/ParameterizedClassTests.java
+++ java/src/test/java/org/sammancoaching/parameterizedtests/concept/ParameterizedClassTests.java
@@ -3,15 +3,7 @@ package org.sammancoaching.parameterizedtests.concept;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.Parameter;
 import org.junit.jupiter.params.ParameterizedClass;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.*;
-
-import java.util.List;
-
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @ParameterizedClass
 @ValueSource(strings = { "foo", "bar", "###", "___", "   " })
