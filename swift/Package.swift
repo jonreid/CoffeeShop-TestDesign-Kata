05daeae950f617f4b9fc0026d507a46c427e43d6
Jon Reid <jon@qualitycoding.org>
2025-07-05 16:14:30 -0700

    Swift: clean up package manifest

--- swift/Package.swift
+++ swift/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,16 +6,13 @@ import PackageDescription
 let package = Package(
     name: "CoffeeShopKata",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CoffeeShopKata",
-            targets: ["CoffeeShopKata"]),
+            targets: ["CoffeeShopKata"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CoffeeShopKata"),
+        .target(name: "CoffeeShopKata"),
         .testTarget(
             name: "CoffeeShopKataTests",
             dependencies: ["CoffeeShopKata"]
